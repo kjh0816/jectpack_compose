02a94b624f316f665b8cdfe8b1ba042064882380
kjh0816 <readshot2@gmail.com>
2021-06-16 18:16:44 +0900

    Column을 통해 텍스트가 UI 상에 곂치지 않도록

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.myapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
@@ -11,19 +12,42 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Gretting("Android")
+            NewsStory()
         }
     }
 }
 
 @Composable
-fun Gretting(name: String){
-    Text(text="yee, $name")
+fun NewsStory() {
+    /*
+    Column(modifier = Modifier.background(Color.Red), content = {
+        Text(text = "A day in Shark Fin Cove")
+        Text(text = "Davenport, California")
+        Text(text = "December 2018")
+    })
+    */
+
+    /*
+    Column(
+        modifier = Modifier
+            .background(Color.Red)
+            .fillMaxWidth()
+    ) {
+        Text(text = "A day in Shark Fin Cove")
+        Text(text = "Davenport, California")
+        Text(text = "December 2018")
+    }
+    */
+
+    Column {
+        Text(text = "A day in Shark Fin Cove")
+        Text(text = "Davenport, California")
+        Text(text = "December 2018")
+    }
 }
 
 @Preview
 @Composable
-fun PriviewGretting(){
-    Gretting("Android")
+fun PreviewApp() {
+    NewsStory()
 }
-
